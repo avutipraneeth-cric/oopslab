1f762649b354d46d86d5dd54f46ddc5ee004b7e9
avutipraneeth-cric <avutipraneeth@gmail.com>
2026-04-02 14:58:04 +0530

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,31 +1,31 @@
-class Calculator {
+public class Calculator {
 
-public int add(int a, int b) {
+    public int add(int a, int b) {
         return a + b;
     }
 
-protected int add(int a, int b, int c) {
+    protected int add(int a, int b, int c) {
         return a + b + c;
     }
 
-private double add(double a, double b) {
+    private double add(double a, double b) {
         return a + b;
     }
-public void displayDecimalSum(double x, double y) {
-        double result = add(x, y);  
-        System.out.println("Sum of decimals: " + result);
+
+    public void displayDecimalAddition(double a, double b) {
+        double result = add(a, b);
+        System.out.println("Decimal Addition Result: " + result);
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        int sum2 = calc.add(10, 20);
+        System.out.println("Two Integers Result: " + sum2);
+
+        int sum3 = calc.add(10, 20, 30);
+        System.out.println("Three Integers Result: " + sum3);
+
+        calc.displayDecimalAddition(10.5, 20.7);
     }
 }
-
-public class Main {
-
-public static void main(String[] args) {
-
-Calculator calc = new Calculator();
-
-System.out.println("Sum of 2 integers: " + calc.add(5, 10));
-
-System.out.println("Sum of 3 integers: " + calc.add(2, 3, 4));
-
-calc.displayDecimalSum(5.5, 2.3);
-    
